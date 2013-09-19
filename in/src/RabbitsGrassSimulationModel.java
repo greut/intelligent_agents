dd422f104e7faea622d50c8d251a405bec31b7af
Yoan Blanc <yoan.blanc@epfl.ch>
2013-09-19 15:47:17 +0200

    Work in progress.

--- in/src/RabbitsGrassSimulationModel.java
+++ in/src/RabbitsGrassSimulationModel.java
@@ -1,5 +1,11 @@
+import java.awt.Color;
+
 import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
+import uchicago.src.sim.gui.ColorMap;
+import uchicago.src.sim.gui.DisplaySurface;
+import uchicago.src.sim.gui.Value2DDisplay;
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -7,40 +13,103 @@ import uchicago.src.sim.engine.SimModelImpl;
  * order to run Repast simulation. It manages the entire RePast
  * environment and the simulation.
  *
- * @author 
+ * @author Yoan Blanc <yoan.blanc@epfl.ch> 
  */
-
-
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
 
-		public static void main(String[] args) {
-			
-			System.out.println("Rabbit skeleton");
-			
-		}
+    private int worldWidth = 20; // 0 - 100
+    private int worldHeight = 20; // 0 - 100
+    private int numberOfRabbits = 150; // 0 - 500
+    private int birthThreshold = 15; // 0 - 20
+    private int grassGrowRate = 15; // 0 - 20
+    private int grassEnergy = 5; // ?
 
-		public void begin() {
-			// TODO Auto-generated method stub
-			
-		}
+    private Schedule schedule;
+    private DisplaySurface displaySurface;
+    private RabbitsGrassSimulationSpace space;
 
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+    public static void main(String[] args) {
+        SimInit init = new SimInit();
+        RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+        init.loadModel(model, "", false);
+    }
 
-		public String getName() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+    public void begin() {
+        // build model
+        space = new RabbitsGrassSimulationSpace(worldWidth, worldHeight);
+        space.spreadGrass(grassGrowRate);
 
-		public Schedule getSchedule() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+        // build schedule
+        // TODO
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			
-		}
+        // build display
+        ColorMap map = new ColorMap();
+        for (int i=1; i<16; i++) {
+            map.mapColor(i, new Color(0, i * 8 + 127, 0));
+        }
+        map.mapColor(0, Color.black);
+        Value2DDisplay displayGrass = new Value2DDisplay(space.getCurrentGrassSpace(), map);
+        displaySurface.addDisplayableProbeable(displayGrass, "Grass");
+        displaySurface.display();
+    }
+
+    /**
+     * @TODO use the parameter file: http://repast.sourceforge.net/repast_3/how-to/params.html
+     */
+    public String[] getInitParam() {
+        return new String[]{ "GridSize",
+                             "Population",
+                             "BirthThreshold",
+                             "GrassGrowRate" };
+    }
+
+    public int getGridSize() {
+        return worldWidth;
+    }
+
+    public void setGridSize(int size) {
+        worldWidth = size;
+        worldHeight = size;
+    }
+
+    public int getPopulation() {
+        return numberOfRabbits;
+    }
+
+    public void setPopulation(int population) {
+        numberOfRabbits = population;
+    }
+
+    public int getBirthThreshold() {
+        return birthThreshold;
+    }
+
+    public void setBirthThreshold(int threshold) {
+        birthThreshold = threshold;
+    }
+
+    public int getGrassGrowRate() {
+        return grassGrowRate;
+    }
+
+    public void setGrassGrowRate(int rate) {
+        grassGrowRate = rate;
+    }
+
+    public String getName() {
+        return "Rabbit";
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setup() {
+        schedule = new Schedule(1);
+        if (displaySurface != null) {
+            displaySurface.dispose();
+        }
+        displaySurface = new DisplaySurface(this, "Window");
+        registerDisplaySurface("Window", displaySurface);
+    }
 }
--- in/src/RabbitsGrassSimulationSpace.java
+++ in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,38 @@
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
- * @author 
+ * @author Yoan Blanc <yoan.blanc@epfl.ch>
  */
-
 public class RabbitsGrassSimulationSpace {
+    private int width;
+    private int height;
 
+    private Object2DGrid grass;
+
+    public RabbitsGrassSimulationSpace(int w, int h) {
+        width = w;
+        height = h;
+        
+        grass = new Object2DGrid(width, height);
+        for (int i=0; i<width; i++) {
+            for (int j=0; j<height; j++) {
+                grass.putObjectAt(i, j, new Integer(0));
+            }
+        }
+    }
+
+    public void spreadGrass(int quantity) {
+        for (int i=0; i<quantity; i++) {
+            int x, y;
+            x = (int) (Math.random() * width);
+            y = (int) (Math.random() * height);
+            grass.putObjectAt(x, y,
+                    ((Integer) grass.getObjectAt(x, y)).intValue() + 1);
+        }
+    }
+
+    public Object2DGrid getCurrentGrassSpace() {
+        return grass;
+    }
 }
