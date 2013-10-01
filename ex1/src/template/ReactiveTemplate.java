df6310d988c40e9173cb11f948933dfd2f565eec
Yoan Blanc <yoan@dosimple.ch>
2013-10-01 19:33:53 +0200

    dos2unix all the things ;-)

--- ex1/src/template/ReactiveTemplate.java
+++ ex1/src/template/ReactiveTemplate.java
@@ -15,31 +15,31 @@ import logist.topology.Topology.City;
 
 public class ReactiveTemplate implements ReactiveBehavior {
 
-	private Random random;
-	private double pPickup;
+    private Random random;
+    private double pPickup;
 
-	@Override
-	public void setup(Topology topology, TaskDistribution td, Agent agent) {
+    @Override
+    public void setup(Topology topology, TaskDistribution td, Agent agent) {
 
-		// Reads the discount factor from the agents.xml file.
-		// If the property is not present it defaults to 0.95
-		Double discount = agent.readProperty("discount-factor", Double.class,
-				0.95);
+        // Reads the discount factor from the agents.xml file.
+        // If the property is not present it defaults to 0.95
+        Double discount = agent.readProperty("discount-factor", Double.class,
+                0.95);
 
-		this.random = new Random();
-		this.pPickup = discount;
-	}
+        this.random = new Random();
+        this.pPickup = discount;
+    }
 
-	@Override
-	public Action act(Vehicle vehicle, Task availableTask) {
-		Action action;
+    @Override
+    public Action act(Vehicle vehicle, Task availableTask) {
+        Action action;
 
-		if (availableTask == null || random.nextDouble() > pPickup) {
-			City currentCity = vehicle.getCurrentCity();
-			action = new Move(currentCity.randomNeighbor(random));
-		} else {
-			action = new Pickup(availableTask);
-		}
-		return action;
-	}
+        if (availableTask == null || random.nextDouble() > pPickup) {
+            City currentCity = vehicle.getCurrentCity();
+            action = new Move(currentCity.randomNeighbor(random));
+        } else {
+            action = new Pickup(availableTask);
+        }
+        return action;
+    }
 }
